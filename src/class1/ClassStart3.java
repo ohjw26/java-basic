1c50d9ec32062cbc3fac56c8d5348d7d37c6fc67
Jiwon Oh <zionyy@skuniv.ac.kr>
2026-02-02 14:52:04 +0900

    Upload Java exercises

--- src/class1/ClassStart3.java
+++ src/class1/ClassStart3.java
@@ -3,7 +3,7 @@ package class1;
 public class ClassStart3 {
     public static void main(String[] args) {
         Student student1;
-        student1 = new Student();
+        student1 = new Student(); // 객체/인스턴스
         student1.name = "학생1";
         student1.age = 15;
         student1.grade = 90;
--- src/class1/ClassStart5.java
+++ src/class1/ClassStart5.java
@@ -1,6 +1,6 @@
 package class1;
 
-public class ClassStart4 {
+public class ClassStart5 {
     public static void main(String[] args) {
         Student student1 = new Student();
         student1.name = "학생1";
@@ -12,11 +12,21 @@ public class ClassStart4 {
         student2.age = 16;
         student2.grade = 80;
 
-        Student[] students = new Student[2];
-        students[0] = student1;
-        students[1] = student2;
+        //student[] students = new Student[]{student1, student2};
+        Student[] students = {student1, student2};
 
-        System.out.println("이름:" + students[0].name + " 나이:" + students[0].age + " 성적:" + students[0].grade);
-        System.out.println("이름:" + students[1].name + " 나이:" + students[1].age + " 성적:" + students[1].grade);
+/*        for (int i = 0; i < students.length; i++) {
+            System.out.println("이름:" + students[i].name + " 나이:" + students[i].age + " 성적:" + students[i].grade);
+        }*/
+
+/*        for (int i = 0; i < students.length; i++) {
+            Student s = students[i];
+            System.out.println("이름:" + s.name + " 나이:" + s.age + " 성적:" + s.grade);
+        }*/
+
+        for (Student s : students) { //iter 단축키
+            System.out.println("이름:" + s.name + " 나이:" + s.age + " 성적:" + s.grade);
+        }
+
     }
 }
