925116924737d6e997f019ff358381a6d3f00926
Jiwon Oh <zionyy@skuniv.ac.kr>
2026-02-27 23:40:06 +0900

    Upload Java exercises

---
+++ src/access/a/AccessInnerMain.java
@@ -0,0 +1,22 @@
+package access.a;
+
+public class AccessInnerMain {
+
+    public static void main(String[] args) {
+        AccessData data = new AccessData();
+
+        //public 호출 가능
+        data.publicField = 1;
+        data.publicMethod();
+
+        //같은 패키지에 defalut 호출 가능
+        data.defalutField = 2;
+        data.defalutMethod();
+
+        //private 호출 불가
+//        data.privateField = 3;
+//        data.privateMethod();
+
+        data.innerAccess();
+    }
+}
