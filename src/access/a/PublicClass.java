159b97995d20c8ec79dd669d6c2263e87e1f5167
Jiwon Oh <zionyy@skuniv.ac.kr>
2026-02-28 17:07:47 +0900

    Upload Java exercises

---
+++ src/access/a/PublicClass.java
@@ -0,0 +1,17 @@
+package access.a;
+
+public class PublicClass {
+    public static void main(String[] args) {
+        PublicClass publicClass = new PublicClass();
+        DefalutClass1 class1 = new DefalutClass1();
+        DefaultCalss2 class2 = new DefaultCalss2();
+    }
+}
+
+class DefalutClass1 {
+
+}
+
+class DefaultCalss2 {
+
+}
---
+++ src/access/a/PublicClassInnerMain.java
@@ -0,0 +1,9 @@
+package access.a;
+
+public class PublicClassInnerMain {
+    public static void main(String[] args) {
+        PublicClass publicClass = new PublicClass();
+        DefalutClass1 class1 = new DefalutClass1();
+        DefaultCalss2 class2 = new DefaultCalss2();
+    }
+}
