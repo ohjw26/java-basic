958894106e6a0ed23eecda07aaef9339f4fca904
Jiwon Oh <zionyy@skuniv.ac.kr>
2026-03-09 23:15:06 +0900

    Upload Java exercises

--- src/static1/Data3.java
+++ src/static1/Data3.java
@@ -1,8 +1,8 @@
 package static1;
 
 public class Data3 {
-    public String name;
-    public static int count; //정적 변수
+    public String name; //인스턴스 변수
+    public static int count; //클래스 변수(정적변수)
 
     public Data3(String name) {
         this.name = name;
