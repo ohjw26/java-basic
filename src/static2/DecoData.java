0cc7ce5a874dc4de412ebf4169476d7c0a29de2c
Jiwon Oh <zionyy@skuniv.ac.kr>
2026-03-11 23:58:58 +0900

    Upload Java exercises

---
+++ src/static2/DecoData.java
@@ -0,0 +1,31 @@
+package static2;
+
+public class DecoData {
+
+    private int instanceValue;
+    private static int staticValue;
+
+    public static void staticCall() {
+        staticValue++; //정적 변수 접근
+        staticMethod(); //정적 메서드 접근
+
+//        instanceValue++;    //인스턴스 변수 접근, compile error
+//        instanceMethod();   //인스턴스 메서드 접근, compile error
+    }
+
+    public void instanceCall() {
+        instanceValue++;    //인스턴스 변수 접근
+        instanceMethod();   //인스턴스 메서드 접근
+
+        staticValue++; //정적 변수 접근 (= DecoData.staticValue++;)
+        staticMethod(); //정적 메서드 접근 (= DecoData.staticMethod();)
+    }
+
+    private void instanceMethod() {
+        System.out.println("instanceValue=" + instanceValue);
+    }
+
+    private static void staticMethod() {
+        System.out.println("staticValue=" + staticValue);
+    }
+}
