d230744a0c3d9c7ef27906b7b0ce4a85b6db6053
Jiwon Oh <ohjiwon3040@gmail.com>
2026-03-12 18:29:58 +0900

    Upload Java exercises

--- src/static2/DecoDataMain.java
+++ src/static2/DecoDataMain.java
@@ -1,10 +1,13 @@
 package static2;
 
+import static static2.DecoData.staticCall;
+
 public class DecoDataMain {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { //메인또한 정적 메서드임
         System.out.println("1. 정적 호출");
-        DecoData.staticCall();
+//        DecoData.staticCall();
+        staticCall();
 
         System.out.println("2. 인스턴스 호출1");
         DecoData data1 = new DecoData();
@@ -13,5 +16,13 @@ public class DecoDataMain {
         System.out.println("3. 인스턴스 호출2");
         DecoData data2 = new DecoData();
         data2.instanceCall();
+
+        //추가
+        //인스턴스를 통한 접근 (권장X)
+        DecoData data3 = new DecoData();
+        data3.staticCall();
+
+        //클래스를 통한 접근
+        staticCall();
     }
 }
