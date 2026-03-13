080cfb86aa1f33679fef7b94359df7c4f171572d
Jiwon Oh <ohjiwon3040@gmail.com>
2026-03-13 15:09:20 +0900

    Upload Java exercises

---
+++ src/final1/FieldInit.java
@@ -0,0 +1,12 @@
+package final1;
+
+public class FieldInit {
+
+    static final int CONST_VALUE = 10;
+    final int value = 10;
+
+    //final 변수의 초기값이 있는 경우에는 생성자 사용이 불가능
+//    public FieldInit() {
+//        this.value = value;
+//    }
+}
